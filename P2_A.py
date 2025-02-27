02cbfd1b0dc4bebcd0ff970023ee7c5e8b7b551b
Jonas Chirinos-sanchez <jonas89jonas89@gmail.com>
2025-02-27 18:38:27 -0500

    Create P2_A.py

---
+++ P2_A.py
@@ -0,0 +1,43 @@
+import console_gfx
+
+def menu():
+      print("\nRLE Menu\n"
+            "________\n"
+            "0. Exit\n"
+            "1. Load File\n"
+            "2. Load Test Image\n"
+            "3. Read RLE String\n"
+            "4. Read RLE Hex String\n"
+            "5. Read Data Hex String\n"
+            "6. Display Image\n"
+            "7. Display RLE String\n"
+            "8. Display Hex RLE Data\n"
+            "9. Display Hex Flat Data\n")
+
+
+def main():
+      print("Welcome to the RLE image encoder!\n")
+      console_gfx.display_image(console_gfx.test_rainbow)
+      while True:
+            menu()
+            option = int(input("Select a Menu Option: "))
+            if option == 0:
+                exit()
+            elif option == 1:
+                  file_name = input("Enter the name of the file: ")
+                  image_data = console_gfx.load_file(file_name)
+            elif option == 2:
+                  image_data = console_gfx.test_image
+                  print("Test image data is loaded")
+            elif option == 3:
+                  pass
+            elif option == 4:
+                  pass
+            elif option == 5:
+                  pass
+            elif option == 6:
+                  console_gfx.display_image(image_data)
+
+
+if __name__ == "__main__":
+      main()
